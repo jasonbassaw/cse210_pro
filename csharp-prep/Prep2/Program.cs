974919ba28449540f40ec932bf382f7810f2931a
jasonbassaw <bas23004@byui.edu>
2024-04-26 13:38:19 -0600

    homework prep2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.ComponentModel;
+using System.Security.Authentication.ExtendedProtection;
+using System.Timers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    
+    
+        Console.Write("What is your percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >=60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        
+        
+        Console.WriteLine($"Your grade is: {letter}");
+        
+
+    
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!");
+        }
+        
+        else 
+        {
+            Console.WriteLine("Sorry, all the best next time");
+        }
+
     }
+
 }
